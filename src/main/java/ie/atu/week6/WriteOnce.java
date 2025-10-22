fad79e8a956ccf4996bb12f7acbf1ecb079a2e00
DanielMaku-STUDENT <G00467390@atu.ie>
2025-10-22 16:41:36 +0100

    finished WriteOnce (i think)

--- src/main/java/ie/atu/week6/WriteOnce.java
+++ src/main/java/ie/atu/week6/WriteOnce.java
@@ -1,4 +1,36 @@
 package ie.atu.week6;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class WriteOnce {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the file name (e.g., students.txt: ");
+        String fileName = sc.nextLine().trim();
+
+        System.out.println("enter a name to save : ");
+        String name = sc.nextLine().trim();
+
+        try(PrintWriter out = new PrintWriter(new FileWriter(fileName,true)))
+        {
+            out.println(name);
+            System.out.println("Saved to" +fileName);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Error" + ex.getMessage());
+        }
+
+
+
+
+
+    }
+
 }
