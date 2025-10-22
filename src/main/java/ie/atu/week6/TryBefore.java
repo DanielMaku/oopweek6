d88d7acabbee1c5266bd276a89b6154814e8ed17
DanielMaku-STUDENT <G00467390@atu.ie>
2025-10-22 16:29:00 +0100

    start of lab

---
+++ src/main/java/ie/atu/week6/TryBefore.java
@@ -0,0 +1,22 @@
+package ie.atu.week6;
+
+import java.util.Scanner;
+
+public class TryBefore {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        System.out.println("enter you age");
+        String text = sc.nextLine().trim();
+
+
+    }
+
+
+
+
+
+
+
+}
